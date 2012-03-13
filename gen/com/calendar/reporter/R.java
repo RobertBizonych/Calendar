91639058cab1e4263031a84cef9f7f9362f0b3e0
RobertBizonych <olderwold@gmail.com>
2012-03-13 14:32:00 +0200

    Adding Projects view

--- gen/com/calendar/reporter/R.java
+++ gen/com/calendar/reporter/R.java
@@ -22,6 +22,7 @@ public final class R {
     public static final class layout {
         public static final int login=0x7f020000;
         public static final int project=0x7f020001;
+        public static final int projects=0x7f020002;
     }
     public static final class string {
         public static final int activities=0x7f03000a;
