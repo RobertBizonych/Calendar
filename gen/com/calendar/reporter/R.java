0a9025542cac24ba4b74cec5a8641d57d4cee56a
RobertBizonych <olderwold@gmail.com>
2012-03-13 14:28:46 +0200

    Adding Login view

--- gen/com/calendar/reporter/R.java
+++ gen/com/calendar/reporter/R.java
@@ -10,10 +10,27 @@ package com.calendar.reporter;
 public final class R {
     public static final class attr {
     }
+    public static final class id {
+        public static final int loginButton=0x7f040002;
+        public static final int passwordField=0x7f040001;
+        public static final int registerButton=0x7f040003;
+        public static final int usernameField=0x7f040000;
+    }
     public static final class layout {
-        public static final int main=0x7f020000;
+        public static final int login=0x7f020000;
     }
     public static final class string {
+        public static final int activities=0x7f03000a;
+        public static final int activityPrompt=0x7f03000b;
         public static final int app_name=0x7f030000;
+        public static final int createProject=0x7f030005;
+        public static final int createTask=0x7f030006;
+        public static final int description=0x7f030004;
+        public static final int login=0x7f030002;
+        public static final int projectDescription=0x7f030008;
+        public static final int projectName=0x7f030007;
+        public static final int register=0x7f030001;
+        public static final int time=0x7f030009;
+        public static final int username=0x7f030003;
     }
 }
--- src/com/calendar/reporter/Login.java
+++ src/com/calendar/reporter/Login.java
@@ -10,6 +10,6 @@ public class Login extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.login);
     }
 }
