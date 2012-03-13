88442b3e7fa26ffcd937df5a3ab4a1550249b8e1
RobertBizonych <olderwold@gmail.com>
2012-03-13 14:19:24 +0200

    Adding task package

---
+++ src/com/calendar/reporter/database/task/TaskDataSource.java
@@ -0,0 +1,79 @@
+package com.calendar.reporter.database.task;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import com.calendar.reporter.database.DataBaseHelper;
+
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+
+public class TaskDataSource {
+    private DataBaseHelper dbHelper;
+
+    private String[] allColumns = {
+            TaskStructure.COLUMN_ID,
+            TaskStructure.COLUMN_NAME,
+            TaskStructure.COLUMN_DESCRIPTION,
+            TaskStructure.COLUMN_DATE,
+            TaskStructure.COLUMN_TIME,
+            TaskStructure.ACTIVITY_ID,
+            TaskStructure.PROJECT_ID
+    };
+
+    public TaskDataSource(Context context) {
+        dbHelper = new DataBaseHelper(context);
+    }
+
+    public TaskStructure createTask(String name, String description, String date, int time, long activityId, long projectId) {
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        try {
+            ContentValues values = new ContentValues();
+
+            values.put(TaskStructure.COLUMN_NAME, name);
+            values.put(TaskStructure.COLUMN_DESCRIPTION, description);
+            values.put(TaskStructure.COLUMN_DATE, date);
+            values.put(TaskStructure.COLUMN_TIME, time);
+            values.put(TaskStructure.ACTIVITY_ID, activityId);
+            values.put(TaskStructure.PROJECT_ID, projectId);
+            long insertId = database.insert(TaskStructure.TABLE_NAME, null,
+                    values);
+            Cursor cursor = database.query(TaskStructure.TABLE_NAME,
+                    allColumns, TaskStructure.COLUMN_ID + " = " + insertId, null,
+                    null, null, null);
+            cursor.moveToFirst();
+            TaskStructure task = cursorToTask(cursor);
+            return task;
+        } finally {
+            database.close();
+        }
+    }
+
+    private TaskStructure cursorToTask(Cursor cursor) {
+        TaskStructure task = new TaskStructure();
+        task.setId(cursor.getLong(0));
+        task.setName(cursor.getString(1));
+        task.setDescription(cursor.getString(2));
+        task.setDate(StringToDate(cursor.getString(3)));
+        task.setTime(cursor.getInt(4));
+        task.setActivityID(cursor.getLong(5));
+        task.setProjectID(cursor.getLong(6));
+        return task;
+    }
+
+    private Date StringToDate(String stringDate) {
+        DateFormat formater = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        try {
+            java.util.Date parsedUtilDate = formater.parse(stringDate);
+            Date sqltDate= new Date(parsedUtilDate.getTime());
+            return sqltDate;
+        } catch (ParseException e) {
+            throw new Error(e.getMessage());
+        }
+    }
+
+}
---
+++ src/com/calendar/reporter/database/task/TaskStructure.java
@@ -0,0 +1,79 @@
+package com.calendar.reporter.database.task;
+
+import java.sql.Date;
+
+public class TaskStructure {
+    public static final String TABLE_NAME = "tasks";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_DESCRIPTION = "description";
+    public static final String COLUMN_TIME = "time";
+    public static final String COLUMN_DATE = "date";
+    public static final String PROJECT_ID = "project_id";
+    public static final String ACTIVITY_ID = "activity_id";
+
+    private long id;
+    private String name;
+    private String description;
+    private int time;
+    private Date date;
+    private long projectID;
+    private long activityID;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public long getProjectID() {
+        return projectID;
+    }
+
+    public void setProjectID(long projectID) {
+        this.projectID = projectID;
+    }
+
+    public long getActivityID() {
+        return activityID;
+    }
+
+    public void setActivityID(long activityID) {
+        this.activityID = activityID;
+    }
+}
