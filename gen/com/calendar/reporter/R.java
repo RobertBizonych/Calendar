265f68a90d1c1e986a7d466749a4881bacaa4ca4
RobertBizonych <olderwold@gmail.com>
2012-03-13 14:30:45 +0200

    Adding Project view

--- gen/com/calendar/reporter/R.java
+++ gen/com/calendar/reporter/R.java
@@ -11,13 +11,17 @@ public final class R {
     public static final class attr {
     }
     public static final class id {
+        public static final int createProject=0x7f040006;
         public static final int loginButton=0x7f040002;
         public static final int passwordField=0x7f040001;
+        public static final int projectDescription=0x7f040005;
+        public static final int projectName=0x7f040004;
         public static final int registerButton=0x7f040003;
         public static final int usernameField=0x7f040000;
     }
     public static final class layout {
         public static final int login=0x7f020000;
+        public static final int project=0x7f020001;
     }
     public static final class string {
         public static final int activities=0x7f03000a;
